b5571818de1b2d46e7d5fcafca6c38fa2fbce6ae
shemanto27 <shemantosharkarofficial@gmail.com>
2024-09-05 01:36:21 +0600

    todo project done

--- 2. Todo List App/todo.js
+++ 2. Todo List App/todo.js
@@ -6,9 +6,37 @@ function addTask(){
         alert('You must write something!');
     }
     else{
+        // task
         let li = document.createElement('li');
         li.innerHTML = inputField.value;
         taskList.appendChild(li);
+
+        // cross icon
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
     }
     inputField.value = '';
+    saveData();
 }
+
+taskList.addEventListener('click',function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem('data', taskList.innerHTML);
+}
+
+function showTask(){
+    taskList.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
